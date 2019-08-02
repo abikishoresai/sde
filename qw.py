2d32e3dc5e363889e9e3eb2b3a68164faa16103e
abikishoresai <52270874+abikishoresai@users.noreply.github.com>
2019-08-02 15:46:47 +0530

    Create qw.py

---
+++ qw.py
@@ -0,0 +1,8 @@
+t=int(input())
+a=[]
+for i in range(t):
+    a.append(input())
+if all("a" in i or "o" in i or "i" in i or "e" in i or "u" in i for i in a):
+    print("yes")
+else:
+    print("no")
